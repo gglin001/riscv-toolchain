db7456bd643c3dcb74d5ae72810f3d6371d17229
Allen Guo <guosonglin001@gmail.com>
2024-06-10 01:33:59 +0800

    wip

--- scripts/llvm_build.sh
+++ scripts/llvm_build.sh
@@ -15,9 +15,9 @@ cmake --build $PWD/build --target install
 # build compiler-rt
 cmake --preset compiler-rt -S$PWD/compiler-rt
 cmake --build $PWD/build_crt --target install
-mkdir -p $PWD/build/install/lib/clang-runtimes/riscv64-unknown-elf/lib
-cp $PWD/build_crt/install/lib/libclang_rt.builtins-riscv64.a \
-  $PWD/build/install/lib/clang-runtimes/riscv64-unknown-elf/lib/libclang_rt.builtins.a
+# mkdir -p $PWD/build/install/lib/clang-runtimes/riscv64-unknown-elf/lib
+# cp $PWD/build_crt/install/lib/libclang_rt.builtins-riscv64.a \
+#   $PWD/build/install/lib/clang-runtimes/riscv64-unknown-elf/lib/libclang_rt.builtins.a
 # clang -target riscv64-unknown-elf -nostartfiles -nostdlib -v ../_demos/hello.c -o hello
 
 # TODO: multi-arch
--- scripts/picolibc_build.sh
+++ scripts/picolibc_build.sh
@@ -20,5 +20,27 @@ meson install -C build
 popd
 
 # TODO: set `--prefix` to llvm toolchain dir
-cp -r $PWD/picolibc/build/install/* \
-  llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/
+# cp -r $PWD/picolibc/build/install/* \
+#   llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/
+# mkdir -p llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d
+# cp -r $PWD/picolibc/build/install/include \
+#   llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/include
+# cp -r $PWD/picolibc/build/install/include \
+#   llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/include
+# cp -r $PWD/picolibc/build/install/lib \
+#   llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/lib
+# cp llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/lib/libclang_rt.builtins.a \
+#   llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/libclang_rt.builtins.a
+
+mkdir -p llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d
+mkdir -p llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/lib
+cp -r $PWD/picolibc/build/install/include \
+  llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/include
+cp -r $PWD/picolibc/build/install/lib/rv64imafdc/lp64d/* \
+  llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/lib/
+cp llvm-project/build_crt/install/lib/libclang_rt.builtins-riscv64.a \
+  llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/lib/libclang_rt.builtins.a
+cp picolibc/build/install/lib/picolibc.ld \
+  llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/lib/picolibc.ld
+cp picolibc/build/install/lib/picolibcpp.ld \
+  llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/lib/picolibcpp.ld
