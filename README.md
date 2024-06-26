a3b2215066d0f4109498c242b9d662f8ce032b2a
Allen Guo <guosonglin001@gmail.com>
2024-06-26 21:59:21 +0800

    wip

--- README.md
+++ README.md
@@ -5,3 +5,24 @@ riscv bare metal toolchain
 ## target
 
 clang + llvm-runtime + picolibc + ld.lld on linux and macos
+
+## build
+
+```bash
+
+# llvm
+bash scripts/llvm_download.sh
+bash scripts/llvm_build.sh
+
+# picolibc
+bash scripts/picolibc_download.sh
+bash scripts/picolibc_build.sh
+
+# optional: newlib
+bash scripts/newlib_download.sh
+bash scripts/newlib_build.sh
+
+# optional: musl
+bash scripts/musl_download.sh
+bash scripts/musl_build.sh
+```
--- scripts/musl_build.sh
+++ scripts/musl_build.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
 
 # $PWD is `.../riscv-toolchain`
-mkdir -p musl/build
 
-pushd musl/build
+pushd musl
 
 LLVM_BINDIR="$PWD/../llvm-project/build/install/bin"
 export PATH="$LLVM_BINDIR:$PATH"
 
+mkdir -p build-rv64imac-lp64 && pushd build-rv64imac-lp64
 CC="clang" CROSS_COMPILE="llvm-" \
-  CFLAGS="--target=riscv64-unknown-elf -mcmodel=medany -nostdlib -O2" \
+  CFLAGS="--target=riscv64-unknown-elf -mcmodel=medany -nostdlib -O2 -march=rv64imac -mabi=lp64" \
   ../configure \
   --prefix=$PWD/install \
   --target=riscv64-unknown-elf \
   --disable-shared
-
 make -j$(nproc)
 make install
+popd
+
+mkdir -p build-rv64imafdc-lp64d && pushd build-rv64imafdc-lp64d
+CC="clang" CROSS_COMPILE="llvm-" \
+  CFLAGS="--target=riscv64-unknown-elf -mcmodel=medany -nostdlib -O2 -march=rv64imafdc -mabi=lp64d" \
+  ../configure \
+  --prefix=$PWD/install \
+  --target=riscv64-unknown-elf \
+  --disable-shared
+make -j$(nproc)
+make install
+popd
 
 popd
 
 # `rv64imac/lp64`
 # no default sysroot is created, set `--sysroot` first
 mkdir -p llvm-project/build/install/lib/musl/riscv64-unknown-elf/rv64imac/lp64
-cp -r musl/build/install/include \
+cp -r musl/build-rv64imac-lp64/install/include \
   llvm-project/build/install/lib/musl/riscv64-unknown-elf/rv64imac/lp64/
-cp -r musl/build/install/lib \
+cp -r musl/build-rv64imac-lp64/install/lib \
   llvm-project/build/install/lib/musl/riscv64-unknown-elf/rv64imac/lp64/
 cp llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imac/lp64/lib/libclang_rt.builtins.a \
   llvm-project/build/install/lib/musl/riscv64-unknown-elf/rv64imac/lp64/lib/
 
 # `rv64imafdc/lp64d`
 mkdir -p llvm-project/build/install/lib/musl/riscv64-unknown-elf/rv64imafdc/lp64d
-cp -r musl/build/install/include \
+cp -r musl/build-rv64imafdc-lp64d/install/include \
   llvm-project/build/install/lib/musl/riscv64-unknown-elf/rv64imafdc/lp64d/
-cp -r musl/build/install/lib \
+cp -r musl/build-rv64imafdc-lp64d/install/lib \
   llvm-project/build/install/lib/musl/riscv64-unknown-elf/rv64imafdc/lp64d/
 cp llvm-project/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/lib/libclang_rt.builtins.a \
   llvm-project/build/install/lib/musl/riscv64-unknown-elf/rv64imafdc/lp64d/lib/
