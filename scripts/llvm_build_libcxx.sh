2caaa0c01d5cc73d831d4759781b8849b102255a
Allen Guo <guosonglin001@gmail.com>
2024-07-08 15:26:25 +0800

    update `CMAKE_SYSROOT`

--- scripts/llvm_build_libcxx.sh
+++ scripts/llvm_build_libcxx.sh
@@ -4,6 +4,14 @@
 
 pushd llvm-project
 
+# build libcxx-rv64imafdc-lp64d
+cmake --preset libcxx-rv64imafdc-lp64d -S$PWD/runtimes -DCMAKE_SYSROOT=$PWD/build/install/lib/clang-runtimes/riscv64-unknown-elf
+cmake --build $PWD/build-libcxx-rv64imafdc-lp64d --target install
+cp -r build-libcxx-rv64imafdc-lp64d/install/include/* \
+  build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/include/
+cp -r build-libcxx-rv64imafdc-lp64d/install/lib/* \
+  build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/lib/
+
 # build libcxx-rv64imac-lp64
 cmake --preset libcxx-rv64imac-lp64 -S$PWD/runtimes -DCMAKE_SYSROOT=$PWD/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imac/lp64
 cmake --build $PWD/build-libcxx-rv64imac-lp64 --target install
@@ -11,16 +19,6 @@ cp -r build-libcxx-rv64imac-lp64/install/include/* \
   build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imac/lp64/include/
 cp -r build-libcxx-rv64imac-lp64/install/lib/* \
   build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imac/lp64/lib/
-
-# build libcxx-rv64imafdc-lp64d
-SYSROOT=$PWD/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d
-export EXTRA_CMAKE_C_FLAGS="-I$SYSROOT/include"
-cmake --preset libcxx-rv64imafdc-lp64d -S$PWD/runtimes -DCMAKE_SYSROOT=$SYSROOT
-cmake --build $PWD/build-libcxx-rv64imafdc-lp64d --target install
-cp -r build-libcxx-rv64imafdc-lp64d/install/include/* \
-  build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/include/
-cp -r build-libcxx-rv64imafdc-lp64d/install/lib/* \
-  build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64imafdc/lp64d/lib/
 
 # build libcxx-rv64ima-lp64
 cmake --preset libcxx-rv64ima-lp64 -S$PWD/runtimes -DCMAKE_SYSROOT=$PWD/build/install/lib/clang-runtimes/riscv64-unknown-elf/rv64ima/lp64
